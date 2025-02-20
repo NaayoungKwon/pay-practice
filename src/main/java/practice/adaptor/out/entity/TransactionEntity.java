b8aa8d14a0615db9580c79337fa865d240817781
naayoungkwon <kny8092@gmail.com>
2025-02-21 00:32:04 +0900

    step1/feat : 계좌 입출금 내역 entity 생성

---
+++ src/main/java/practice/adaptor/out/entity/TransactionEntity.java
@@ -0,0 +1,46 @@
+package practice.adaptor.out.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Entity(name = "transaction")
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@EntityListeners(value = AuditingEntityListener.class)
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class TransactionEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  Long id;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "account_id")
+  AccountEntity account;
+  String transactionType;
+  BigDecimal amount;
+  LocalDate createdDate;
+
+  @CreatedDate
+  LocalDateTime createdAt;
+
+}
