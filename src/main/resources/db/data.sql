01721e4e79baedf40a093299a590dc177d461e69
naayoungkwon <kny8092@gmail.com>
2025-02-22 19:13:47 +0900

    step1/setting : db mysql로 변경

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -1,6 +1,12 @@
+DELETE FROM transaction;
+DELETE FROM account;
+DELETE FROM users;
+
 INSERT INTO users (id, name, email, created_at, updated_at) values (1, 'nayoung', 'test@gmail.com',CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
+INSERT INTO users (id, name, email, created_at, updated_at) values (2, 'hyun', 'hyun@gmail.com',CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
 
 INSERT INTO account (id, user_id, type, balance, created_at, updated_at) values (1, 1, 'MAIN', 10000, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
 INSERT INTO account (id, user_id, type, balance, created_at, updated_at) values (2, 1, 'SAVINGS', 0, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
 
-INSERT INTO transaction (id, account_id, transaction_type, amount, created_at, created_date) values (1, 1, 'WITHDRAW', 1000, CURRENT_TIMESTAMP(), current_date());
+INSERT INTO account (id, user_id, type, balance, created_at, updated_at) values (3, 2, 'MAIN', 10000, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
+INSERT INTO account (id, user_id, type, balance, created_at, updated_at) values (4, 2, 'SAVINGS', 0, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
