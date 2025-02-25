209c59ad54942557df0c742efa316788e20a39b4
naayoungkwon <kny8092@gmail.com>
2025-02-26 00:59:11 +0900

    step1/feat : 일일 인출 한도 TTL 적용

--- src/main/java/practice/adaptor/out/TodayExternalWithdrawRepository.java
+++ src/main/java/practice/adaptor/out/TodayExternalWithdrawRepository.java
@@ -4,6 +4,7 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Optional;
+import java.util.concurrent.TimeUnit;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
@@ -13,11 +14,16 @@ import org.springframework.stereotype.Component;
 public class TodayExternalWithdrawRepository {
 
   private final RedisTemplate<String, Integer> redisTemplate;
+  private static final Integer TTL_HOURS = 24;
 
   public Integer get(Long accountId) {
     String key = getKey(accountId);
-    return Optional.ofNullable(redisTemplate.opsForValue().get(key))
-        .orElse(0);
+
+    Optional<Integer> value = Optional.ofNullable(redisTemplate.opsForValue().get(key));
+    return value.orElseGet(() -> {
+      redisTemplate.opsForValue().set(key, 0, TTL_HOURS, TimeUnit.HOURS);
+      return 0;
+    });
   }
 
   private String getKey(Long accountId){
